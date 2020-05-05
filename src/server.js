c718a90b1f0f959984fa358afcaf6181685c336c
Tyler Sharp <johntylersharp@gmail.com>
2020-05-05 11:36:47 -0400

    Exercise 1 and 2 complete

--- src/server.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-const { PORT, NODE_ENV } = require("./config");
-
-app.listen(PORT, () => console.log(`Server running in ${NODE_ENV} on ${PORT}`));
--- test/app.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app');
-
-describe('App', () => {
-  it('should return 200 "Hello world!"', () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, {message: 'Hello world!' });
-  });
-});
